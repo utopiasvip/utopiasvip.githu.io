5f641b8ba4e8afffddf390c8b4ce7f0abf01b9f1
utopia <>
2020-04-18 02:07:35 +0800

    Update English

---
+++ precache-manifest.0dd0817d94ed5be2c2759d9cc212adf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce443b7d7ff1693c147",
+    "url": "/css/app.113e99f1.css"
+  },
+  {
+    "revision": "6e5a349c248e497db526",
+    "url": "/css/chunk-vendors.e6568296.css"
+  },
+  {
+    "revision": "bc44bd9340e0bde5247b28e23ea4162b",
+    "url": "/img/home_logo.bc44bd93.png"
+  },
+  {
+    "revision": "cf652b8018f0014e35c7c0ce072cfe74",
+    "url": "/img/index_bg.cf652b80.png"
+  },
+  {
+    "revision": "b9bfedc05e23d55c775bdf5a5deb2304",
+    "url": "/img/power_logo.b9bfedc0.png"
+  },
+  {
+    "revision": "70938ee0aaebf4d2daccacd2783b3c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce443b7d7ff1693c147",
+    "url": "/js/app.47632fc3.js"
+  },
+  {
+    "revision": "6e5a349c248e497db526",
+    "url": "/js/chunk-vendors.f69fd97f.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
